f01d6762f3cdee4c396ca663e8390df73c141a49
Nordyun <njorthson@proton.me>
2026-04-22 12:10:10 -0400

    letta fixes

--- hosts/anubis/letta.nix
+++ hosts/anubis/letta.nix
@@ -5,16 +5,15 @@
 
   age.secrets.letta-env.file = ../../secrets/letta-env.age;
 
-  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 8283 6333 ];
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 8283 ];
 
   systemd.tmpfiles.rules = [
     "d /mercury/letta 0755 root root - -"
     "d /mercury/letta/app 0700 root root - -"
-    "d /mercury/letta/qdrant 0700 root root - -"
   ];
 
   virtualisation.oci-containers.containers."letta" = {
-    image = "letta/letta:latest";
+    image = "docker.io/letta/letta:latest";
     environmentFiles = [ config.age.secrets.letta-env.path ];
     volumes = [
       "/mercury/letta/app:/root/.letta:rw"
@@ -28,22 +27,7 @@
     };
   };
 
-  virtualisation.oci-containers.containers."qdrant" = {
-    image = "qdrant/qdrant:latest";
-    volumes = [
-      "/mercury/letta/qdrant:/qdrant/storage:rw"
-    ];
-    ports = [
-      "6333:6333/tcp"
-    ];
-    log-driver = "journald";
-    labels = {
-      "io.containers.autoupdate" = "registry";
-    };
-  };
-
   systemd.services."podman-letta".serviceConfig.Restart = lib.mkOverride 90 "always";
-  systemd.services."podman-qdrant".serviceConfig.Restart = lib.mkOverride 90 "always";
 
   systemd.services.podman-auto-update = {
     description = "Podman auto-update service";
