3c967d388b0536afd6d800910314c7830573ea08
nordyun <njorthson@proton.me>
2026-09-09 22:14:49 -0400

    add horus keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -10,7 +10,7 @@ let
   neptune = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGaDdqPC3F7hfYYU4b181GxcLkAZyTBAWHJ23hUWiI3";
   zelda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQ/40iaZdCUOK24lAyPmyt1SJVaLKGQK50FZCm5Mzbt";
   nixmacVM = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL4dWVZcNnAXGKgF0ZlzGCIkD93pODqU05qH7RzhPIWv";
-  horus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHBnWNrkuuvLlGjJJBJ0VxXTbm+8t40UxEqWQCGk3fFm";
+  horus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVkkDUut8az0TvR48sZCqoJVssvsw/Lu5ibSOsm3kQ0";
   # TODO(thoth): after install, add thoth's host key here (from
   # /etc/ssh/ssh_host_ed25519_key.pub on thoth), add `thoth` to `systems`
   # below and to the per-secret lists it needs (tailscale_key, washpw,
