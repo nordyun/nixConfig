c36052f9e904fe30936d57aecfdb78c1e20fdfa4
nordyun <njorthson@proton.me>
2026-09-13 02:53:55 +0000

    fix dhcp reservation for thoth

--- hosts/neptune/router.nix
+++ hosts/neptune/router.nix
@@ -71,11 +71,11 @@
               ip-address = "10.1.1.2";
               hostname = "tp-link";
             }
-            # {
-            #   hw-address = "9c:6b:00:af:e9:f7";
-            #   ip-address = "10.1.1.3";
-            #   hostname = "thoth";
-            # }
+            {
+              hw-address = "9c:6b:00:af:e9:f7";
+              ip-address = "10.1.1.3";
+              hostname = "thoth";
+            }
             {
               hw-address = "40:b0:76:d9:d2:5c";
               ip-address = "10.1.1.4";
